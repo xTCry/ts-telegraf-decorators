c2fe7ed8372416b23a0afe0159b9cb87afae350e
xTCry [Vladislav Kh] <wolt6@mail.ru>
2021-04-21 20:56:58 +0300

    chore: fix & reafact

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,32 @@
 import * as path from 'path';
-import Telegraf from 'telegraf';
+import Telegraf, { Context } from 'telegraf';
 import * as glob from 'glob';
 import { buildFromMetadata } from './builder';
 import { getContainer, useContainer } from './container';
 import { IBotOptions } from './interfaces/IBotOptions';
 
-export function buildBot(options: IBotOptions) {
-    if (options.container) useContainer(options.container);
-    let bot = options.bot || new Telegraf(options.token);
+export function buildBot<TC extends Context = Context>(options: IBotOptions<TC>) {
+    if (options.container) {
+        useContainer(options.container);
+    }
 
+    let bot = options.bot || new Telegraf<TC>(options.token);
     getContainer().set(Telegraf, bot);
 
-    if (!(options.controllers as any[]).every((value) => value instanceof Function))
-        (options.controllers as string[]).forEach((value) =>
+    (options.controllers as any[])
+        .filter((value) => !(value instanceof Function))
+        .forEach((value: string) =>
             glob
                 .sync(path.normalize(value))
                 .filter((file) => file.substring(file.length - 5, file.length) !== '.d.ts')
                 .forEach((dir) => require(dir)),
         );
 
-    return buildFromMetadata(bot, options);
+    return buildFromMetadata<TC>(bot, options);
 }
 
 export * from './decorators';
 export * from './interfaces/IBotOptions';
 export * from './metadata';
 export * from './MetadataStorage';
+export * from './TFIMiddleware';
--- src/interfaces/IBotOptions.ts
+++ src/interfaces/IBotOptions.ts
@@ -1,8 +1,8 @@
-import { Middleware, Context } from 'telegraf';
+import Telegraf, { Middleware, Context } from 'telegraf';
 import { Container } from '../container';
 
-export interface IBotOptions {
-    bot?: any;
+export interface IBotOptions<TC extends Context = Context> {
+    bot?: Telegraf<TC>;
     token?: string;
     container?: Container;
     controllers: Function[] | string[];
